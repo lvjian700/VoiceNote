0c8d9fdf067bc7bc0c240dfe268f1e0824ea014b
Jian Lv <lvjian700@gmail.com>
2023-06-12 21:34:13 +1000

    Initial Commit

---
+++ VoiceNote/VoiceNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceNoteApp.swift
+//  VoiceNote
+//
+//  Created by JIAN LYU on 12/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct VoiceNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceNoteUITests/VoiceNoteUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VoiceNoteUITestsLaunchTests.swift
+//  VoiceNoteUITests
+//
+//  Created by JIAN LYU on 12/6/2023.
+//
+
+import XCTest
+
+final class VoiceNoteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
