9355d77a20c9636c7a52cf4760282b6fec11c3a9
Jian Lv <lvjian700@gmail.com>
2023-06-12 22:38:06 +1000

    Build PoC

--- VoiceNote/ContentView.swift
+++ VoiceNote/ContentView.swift
@@ -8,19 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+  @StateObject private var speechRecognizer = SpeechRecognizer()
+  
+  var body: some View {
+    VStack {
+      Text(speechRecognizer.text)
+        .font(.largeTitle)
         .padding()
+      
+      Button(speechRecognizer.isRecording ? "Stop" : "Record") {
+        speechRecognizer.isRecording ? speechRecognizer.stopRecording() : speechRecognizer.startRecording()
+      }
+      .font(.title)
+      .padding()
+      .background(Color.blue)
+      .foregroundColor(.white)
+      .clipShape(Capsule())
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
---
+++ VoiceNote/SpeechRecognizer.swift
@@ -0,0 +1,65 @@
+import SwiftUI
+import Speech
+
+class SpeechRecognizer: ObservableObject {
+  private let speechRecognizer = SFSpeechRecognizer()
+  private let audioEngine = AVAudioEngine()
+  private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+  private var recognitionTask: SFSpeechRecognitionTask?
+  
+  @Published var isRecording = false
+  @Published var text = ""
+  
+  func startRecording() {
+    isRecording = true
+    text = ""
+    
+    do {
+      let audioSession = AVAudioSession.sharedInstance()
+      try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+      try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+    } catch {
+      print("Failed to setup audio session: \(error)")
+    }
+    
+    recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+    
+    guard let recognitionRequest = recognitionRequest else {
+      print("Unable to created a SFSpeechAudioBufferRecognitionRequest object")
+      return
+    }
+    
+    recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest) { result, error in
+      if let result = result {
+        self.text = result.bestTranscription.formattedString
+      }
+      
+      if let error = error {
+        print("Recognition failed: \(error)")
+        self.stopRecording()
+      }
+    }
+    
+    let recordingFormat = audioEngine.inputNode.outputFormat(forBus: 0)
+    
+    audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+      self.recognitionRequest?.append(buffer)
+    }
+    
+    audioEngine.prepare()
+    
+    do {
+      try audioEngine.start()
+    } catch {
+      print("Failed to start audio engine: \(error)")
+    }
+  }
+  
+  func stopRecording() {
+    isRecording = false
+    audioEngine.stop()
+    audioEngine.inputNode.removeTap(onBus: 0)
+    recognitionRequest = nil
+    recognitionTask = nil
+  }
+}
